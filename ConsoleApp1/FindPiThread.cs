28cec461e5727795c5dafce94ddda3d84eccca65
Jeffrey Jordan <jefljord@ut.utm.edu>
2019-10-10 19:05:14 -0500

    Add project files.

---
+++ ConsoleApp1/FindPiThread.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FindPiThread
+    {
+        public int numOfDarts;
+        public int hitCounter;
+        Random x;
+
+        public FindPiThread(int darts)
+        {
+            numOfDarts = darts;
+            hitCounter = 0;
+            x = new Random();
+            double hit = x.NextDouble();
+            Console.WriteLine(hit);
+            Console.ReadKey();
+        }
+    }
+}
